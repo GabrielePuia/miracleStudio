2976e1f6f3d822844f5a36f63b1565a8a4497601
GabrielePuia <51226246+GabrielePuia@users.noreply.github.com>
2022-08-10 01:51:25 +0200

    home statica

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,55 +1,90 @@
-var menuButton = $('.menu-icon');
-var menu = $('#menu');
+var header, menuToggle, menuToggleCheckbox, menuContainer, tlOpeningMenu, tlClosingMenu;
 
-var portfolioHomeSection = $('.portfolio__container');
+header = document.getElementsByTagName("header")[0];
+menuToggle = document.getElementById("menu-toggle");
+menuToggleCheckbox = document.getElementById("menu-toggle-checkbox");
+menuToggleCheckbox.checked = false;
+menuContainer = document.getElementById("menu");
+menuBody = document.getElementById("menu-body");
+menuTopBorder = menuBody.querySelector("hr");
 
-var project = $('.project');
+tlOpeningMenu1 = gsap.timeline();
+tlOpeningMenu2 = gsap.timeline();
+tlClosingMenu1 = gsap.timeline();
+tlClosingMenu2 = gsap.timeline();
 
-var galleryImage = $('.gallery__img');
-
-function openCloseMenu() {
-    if(menu.hasClass('collapsed')) {
-        menu.removeClass('collapsed');
-    } else {
-        menu.addClass("collapsed");
+function showHideMenu() {
+    if (menuToggleCheckbox.checked) {//OPEN MENU
+        gsap.to(header, { duration: 0.01, backgroundColor: "#111111" });
+        tlOpeningMenu1
+            .to(menuContainer, { duration: 0.01, display: "flex" })
+            .to(menuContainer, { duration: 0.6, opacity: "1" })
+            .to(menuTopBorder, { duration: 1, ease: "power4.in", width: "100%" })
+            .from(".menu__body .row .menu__links-column .menu__links li a", 0.8, {
+                y: 100,
+                opacity: 0,
+                ease: "power4.out",
+                stagger: {
+                    amount: 0.3
+                }
+            });
+        tlOpeningMenu2
+            .to(menuToggle, { duration: 0.61, pointerEvents: "none" })
+            .from(".menu__body .row .menu__content-column .menu__content li *", 0.8, {
+                y: 200,
+                opacity: 0,
+                ease: "power4.out",
+                stagger: {
+                    amount: 0.3
+                }
+            })
+            .to(menuToggle, { duration: 0.61, pointerEvents: "auto" });
+    } else {//CLOSEN MENU
+        gsap.to(menuToggle, { duration: 0.01, pointerEvents: "none" });
+        tlClosingMenu1
+            .to(".menu__body .row .menu__links-column .menu__links li a", 0.8, {
+                y: 100,
+                opacity: 0,
+                ease: "power4.in",
+                stagger: {
+                    amount: 0.3
+                }
+            })
+            .to(header, { duration: 1, opacity: "1" })
+            .to(header, { duration: 0.01, backgroundColor: "rgba(0,0,0,0)" });
+        tlClosingMenu2
+            .to(menuTopBorder, { duration: 1, ease: "power4.in", width: "0" })
+            .to(".menu__body .row .menu__content-column .menu__content li *", 0.8, {
+                y: 200,
+                opacity: 0,
+                ease: "power4.in",
+                stagger: {
+                    amount: 0.3
+                }
+            })
+            .to(menuContainer, { duration: 0.6, opacity: "0" })
+            .to(menuContainer, { duration: 0.01, display: "none" })
+            //ritorna allo stato normale per poter fare l'animazione dopo
+            .to(".menu__body .row .menu__links-column .menu__links li a", 0.1, {
+                y: 0,
+                opacity: 1,
+                ease: "power4.in"
+            })
+            .to(".menu__body .row .menu__content-column .menu__content li *", 0.1, {
+                y: 0,
+                opacity: 1,
+                ease: "power4.in"
+            })
+            .to(menuToggle, { duration: 0.01, pointerEvents: "auto" });
     }
 }
 
-$(document).ready(function() {
+window.onload = function (event) {
+    //Current year 
+    var currentYearSpan = document.getElementById("current-year");
+    currentYearSpan.innerHTML = new Date().getFullYear();
 
-    $("#year").text( (new Date).getFullYear() );
-
-    menuButton.on( "click", openCloseMenu );
-    //Codice in home
-    if($(".homepage__hero").length > 0) {
-        portfolioHomeSection.mouseover(function(){
-            $(this).find(".btn").addClass("btn--revealed");
-        });
-        portfolioHomeSection.mouseout(function(){
-            $(this).find(".btn").removeClass("btn--revealed");
-        });
-    }
-    //Codice in pagina contenente elenco progetti
-    /*if($('.projects__container').length > 0) {
-        project.mouseover(function() {
-            $(this).find('.project__content').addClass("project__content--expanded");
-            $(this).find('.project__footer').addClass("project__footer--revealed");
-        });
-        project.mouseout(function() {
-            $(this).find('.project__content').removeClass("project__content--expanded");
-            $(this).find('.project__footer').removeClass("project__footer--revealed");
-        });
-    }*/
-    //Codice dove c'è una galleria
-    if($('.gallery').length > 0) {
-        galleryImage.mouseenter(function(){
-            $(this).append('<span class="img__caption">' + $(this).attr("data-caption") + '</span>');
-            //$(this).find('.img__caption').css("opacity", "1");
-            $(this).find('.img__caption').hide();
-            $(this).find('.img__caption').fadeIn();
-        });
-        galleryImage.mouseleave(function(){
-            $(this).find(".img__caption").remove();
-        });
-    }
-});
+    menuToggle.addEventListener("click", function () {
+        showHideMenu();
+    });
+};
